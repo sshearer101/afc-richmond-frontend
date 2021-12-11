555977627ea22ca04eedc4ba46dd0457a2791025
A Joseph <alecrewinkel@gmail.com>
2021-12-11 12:51:52 -0700

    routing and pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
 
-function App() {
+import { Routes, Route } from 'react-router-dom';
+import ClubHistory from './components/clubHistory';
+import Characters from './components/Characters';
+import TeamQuiz from './components/TeamQuizProfile';
+import HateNate from './components/hateNate';
+
+class App extends React.Component {
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div>
+
+    <h1>A.F.C. Richmond</h1>
+    <header>
+    <nav>
+      <ul>
+        <li><a href="/history">History</a></li>
+        <li><a href="/characters">Characters</a></li>
+        <li><a href="quiz">Quiz</a></li>
+        <li><a href="hateNate">HateNate</a></li>
+        <li><a href="/">Home</a></li>
+      </ul>
+    </nav>
+    </header>
+    {/* Uses a header that scrolls with the text, rather than staying locked at the top */}
+    <div className="demo-big-content">
+
+    
+        <Routes>
+            <Route path="/history" element={<ClubHistory/>}/>
+        </Routes>
+        <Routes>
+            <Route path="/characters" element={<Characters/>}/>
+        </Routes>
+        <Routes>
+            <Route path="/quiz" element={<TeamQuiz/>}/>
+        </Routes>
+        <Routes>
+            <Route path="/hateNate" element={<HateNate/>}/>
+        </Routes>
+
     </div>
+  </div>
   );
+}
 }
 
 export default App;
---
+++ src/components/Characters.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+
+class Characters extends React.Component {
+    render() {
+        return(
+            <div>
+                <h1>
+                 Here are all of your favorite players.
+                </h1>
+            </div>
+        );
+    }
+}
+
+export default Characters;

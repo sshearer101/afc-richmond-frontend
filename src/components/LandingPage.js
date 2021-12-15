4b46f621880445d2574b6cd16a30ac5da80881d8
A Joseph <alecrewinkel@gmail.com>
2021-12-15 14:31:18 -0700

    navbar changes

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React from "react";
 
-import Team from '../Media/Images/AFCteamphoto.jpeg'
+import Team from "../Media/Images/teamafc.jpeg";
 
 class LandingPage extends React.Component {
-    render() {
-        return(
-            <div>
-                <h1>
-                 Welcome to the dog track.
-                </h1>
-                <img src={Team} alt='team of people'/>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h1>Welcome to the dog track.</h1>
+        <img
+          className="teamPhoto"
+          src={Team}
+          width="100%"
+          alt="team of people"
+        />
+      </div>
+    );
+  }
 }
 
 export default LandingPage;

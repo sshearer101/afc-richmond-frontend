7ef81516c56b34fee3d6b1c1d7361254d5b0510e
Sam Shearer <sshearer101@gmail.com>
2022-01-03 18:48:15 -0700

    character styling

--- src/App.js
+++ src/App.js
@@ -1,64 +1,41 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 import "./App.css";
 
-import { Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import ClubHistory from "./components/ClubHistory";
-import Characters from "./components/Characters";
+import CharactersPage from "./components/CharactersPage";
 import TeamQuiz from "./components/TeamQuizProfile";
 import HateNate from "./components/HateNate";
 import LandingPage from "./components/LandingPage";
 import NavBar from './components/NavBar';
 
-// const myAPI = 'http://localhost:4000'
+// const myAPI = 'http://localhost:3000'
 
 function App() {
   ///** Below was trying to set the state up to import the react library for a dropdown not done yet. */
   // const [numberOfItems, setNumberOfItems] = useState(3);
   // const { buttonProps, itemProps, isOpen } = useDropdownMenu(numberOfItems);
 
-  const [characters, setCharacters] = useState([])
+  const [characters, setCharacters] = useState([]);
 
   useEffect(() =>
-      fetch (`http://localhost:4000/characters`)
+      fetch (`/characters`)
         .then((res) => res.json())
-        .then((data) => console.log(data, "wekjfnwekjfn"))
-    , [],
-  )
+        .then((json) => setCharacters(json))
+    , [], 
+  );
 
-  /*const [state, setState] = useState({
-    show: "false",
-    //Need to set state to false so that when clicked it gets changed to true and dropdown appears.
-  });
-  const show = state.show;
-  function open() {
-    console.log(state.show + "it works");
-    setState("true");
-    console.log(state.show + "now");
-  }
 
-  function close() {
-    setState(false);
-  }*/
-
-  
   return (
     <div>
       <NavBar /> 
       <div className="demo-big-content">
     
         <Routes>
-          <Route path="/history" element={<ClubHistory />} />
-        </Routes>
-        <Routes>
-          <Route path="/characters" element={<Characters />} />
-        </Routes>
-        <Routes>
+          <Route path="/history" element={<ClubHistory/>} />
+          <Route path="/characters" element={<CharactersPage characters={characters}/>} />
           <Route path="/quiz" element={<TeamQuiz />} />
-        </Routes>
-        <Routes>
-          <Route path="/hateNate" element={<HateNate />} />
-        </Routes>
-        <Routes>
+          <Route path="/hatenate" element={<HateNate />} />
           <Route path="/" element={<LandingPage />} />
         </Routes>
       </div>
---
+++ src/components/position components/MidfielderContainer.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function MidfielderContainer({ character }) {
+  if (character.field_placement === 'Midfielder') {
+    return (
+      <div className="player-card">
+        <img
+          src={character.image}
+          alt="character-image"
+          className="character-img"
+        />
+        <h1 className="char-name-black">{character.full_name}</h1>
+        <h3 className="player-position-black">{character.position}</h3>
+        <div className="number-flag">
+          <h3 className="jersey-number-black">{character.jersey_number}</h3>
+          <img src={character.flag} alt="character-flag" className="flag-img" />
+        </div>
+      </div>
+    )
+  } else {
+    return null
+  }
+}
+
+export default MidfielderContainer

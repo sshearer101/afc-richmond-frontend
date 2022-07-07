54a327a316cbeac4b39e1eb4c86014dc3f81cbcf
Sam Shearer <85294886+sshearer101@users.noreply.github.com>
2022-07-07 16:11:02 -0600

    quiz

--- src/components/TeamQuiz.js
+++ src/components/TeamQuiz.js
@@ -6,32 +6,30 @@ function TeamQuiz({questions}) {
   const [currentQuestion, setCurrentQuestion] = useState(0)
   const [showScore, setShowScore] = useState(false)
   const [score, setScore] = useState(0)
+  const [answerChoice, setAnswerChoice] = useState()
 
-  const [answer, setAnswer] = useState()
 
     if(questions.length < 1){
         return "Loading..."
     }
 
+// function handleClick(x){
+//   setAnswerChoice(x)
+// }
 
-console.log(questions)
-
-
-function handleAnswer(x){
-  console.log(x)
-//   if(x === true){
-//       setScore(score + 1)
-//   }
-//   const nextQuestion = currentQuestion + 1
-//   if(nextQuestion < questions.length){
-//       setCurrentQuestion(nextQuestion)
-//   }
-//   else{
-//       setShowScore(true)
-//   }
+function handleClick(e){
+  if(e === true){
+      setScore(score + 1)
+  }
+  const nextQuestion = currentQuestion + 1
+  if(nextQuestion < questions.length){
+      setCurrentQuestion(nextQuestion)
+  }
+  else{
+      setShowScore(true)
+  }
 }
 
-const quiz_answers = questions[currentQuestion].quiz_answer 
 
 return(
   <div>
@@ -45,34 +43,15 @@ return(
               </div>
               <div className='question-text'>{questions[currentQuestion].question}</div>
           </div>
-          <div className='answer-div'>
-            <form onSubmit={handleAnswer}>
-              <input
-                type="button"
-                // value={questions[currentQuestion].quiz_answer.answer_one}
-                onClick={()=>setAnswer(questions[currentQuestion].quiz_answer.answer_one)}
-              >
-              </input>
-            </form>
-    
-            {/* <input>{questions[currentQuestion].quiz_answer.answer_one}</input>
-              <input>{questions[currentQuestion].quiz_answer.answer_two}</input>
-              <input>{questions[currentQuestion].quiz_answer.answer_three}</input>
-              <input>{questions[currentQuestion].quiz_answer.answer_four}</input> */}
-
-
-              
-              {/* {Object.keys(quiz_answers).map(function(k, i){
-              })} */}
-
-              {/* {questions[currentQuestion].quiz_answer.map((x, i) => 
+  
+              {questions[currentQuestion].quiz_answer.map((x, i) => 
                   <button 
                       key={i} 
-                      onClick={() => handleAnswer(x.isCorrect)}
+                      onClick={() => handleClick(x.is_correct)}
                   
-                  >{x.quiz_answer}</button>
-              )} */}
-          </div>
+                  >{x.answer}</button >
+              )}
+      
       </>
       )
   }
